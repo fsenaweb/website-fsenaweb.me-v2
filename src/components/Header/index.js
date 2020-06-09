6a506163bbcb705c359cf7204a51bb5e79074735
Matheus Ricelly <fsenaweb@gmail.com>
2020-06-08 22:46:25 -0300

    Estruturação do Header, sem as animações e responsividade

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,48 @@
 import React from "react"
 import Photo from './photo'
 
+import GlobalStyles from "../../styles/global";
+
 import * as S from './styled'
 
 const Header = () => (
   <S.Wrapper>
+    <GlobalStyles/>
     <S.Container>
-      <S.Logo>
-        fsenaweb<span>.me</span>
-      </S.Logo>
-      <S.HeaderPhoto>
-        <Photo/>
-      </S.HeaderPhoto>
+      <S.Top>
+        <S.Logo>
+          fsenaweb<span>.me</span>
+        </S.Logo>
+        <S.Menu>
+          <ul>
+            <li>Quem sou</li>
+            <li>Portfólio</li>
+            <li>Contatos</li>
+          </ul>
+        </S.Menu>
+      </S.Top>
+      <S.Content>
+        <S.HeaderPhoto>
+          <Photo/>
+        </S.HeaderPhoto>
+        <S.Description>
+          <S.DescriptionText>
+            Desenvolvendo as melhores soluções web para o sucesso da sua empresa
+          </S.DescriptionText>
+          <S.SkillText>
+            Progressive Web App - Sistemas Web - Landing Page
+            Single Page Application
+          </S.SkillText>
+          <S.SocialMedia>
+            <S.Linkedin />
+            <S.Medium />
+            <S.Git />
+            <S.Twit />
+            <S.Insta />
+            <S.Face />
+          </S.SocialMedia>
+        </S.Description>
+      </S.Content>
     </S.Container>
   </S.Wrapper>
 )
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,45 +1,204 @@
 import styled from 'styled-components'
 import BgHeader from '../../images/header-bg.svg';
 
+import { LinkedinSquare, MediumSquare, InstagramAlt, FacebookSquare, Github, Twitter } from '@styled-icons/boxicons-logos';
+
+const fontFamily = 'Roboto, sans-serif'
+
 export const Wrapper = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500;700;900&display=swap');
+  font-family: ${fontFamily};
   width: 100vw;
   height: 768px;
   position: relative;
   background-image: url(${BgHeader});
-  background-position: bottom;
+  background-position: bottom left;
   background-repeat: repeat-x;
 `;
 
+export const Container = styled.div`
+  width: 85%;
+  height: 790px;
+  margin: 0 auto;
+  padding-top: 20px;
+`;
+
+export const Top = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 export const Logo = styled.h1`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-grow: 1;
   width: 40%;
   height: 50px;
-  font-weight: bold;
-  letter-spacing: -1px;
+  font-family: ${fontFamily};
+  font-size: 2.5em;
+  font-weight: 900;
+  letter-spacing: -1.5px;
   color: #fff;
-  padding: 30px 40px;
-  
+  padding: 30px 45px;
   & span {
+    font-size: 0.7em;
     color: #638DB3;
+    padding-top: 10px;
   }
 `;
 
-export const Container = styled.div`
-  width: 80%;
-  height: 790px;
-  margin: 0 auto;
+export const Menu = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-grow: 2;
+  width: 60%;
+  height: 50px;
+  font-family: ${fontFamily};
+  font-weight: 900;
+  letter-spacing: -1.5px;
+  font-size: 1.5em;
+  padding: 30px 45px;
+  color: #FFFFFF;
+  ul {
+    list-style: none;
+    li {
+      display: inline;
+      margin-left: 45px;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  width: 100%;
+  height: 688px;
+  position: relative;
 `;
 
 export const HeaderPhoto = styled.div`
   width: 592px;
   height: 688px;
-  position: absolute;
-  margin-left: 30px;
-  bottom: -50px;
+  margin-top: 50px;
+`;
+
+export const Description = styled.div`
+  display: block;
+  width: 85%;
+  margin-left: 50px;
+`;
+
+export const DescriptionText = styled.div`
+  width: 90%;
+  height: auto;
+  font-family: ${fontFamily};
+  font-weight: 700;
+  font-size: 2.5em;
+  letter-spacing: -2px;
+  line-height: 1.2em;
+  color: #FFFFFF;
+  margin-top: 100px;
+  margin-bottom: 20px;
+`;
+
+export const SkillText = styled.div`
+  width: 90%;
+  height: auto;
+  font-family: ${fontFamily};
+  font-weight: 300;
+  font-size: 1.5em;
+  letter-spacing: -1px;
+  line-height: 1.5em;
+  margin-bottom: 50px;
+  color: #638DB3;
+`;
+
+export const SocialMedia = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 50px;
+  padding-left: 80px;
+`;
+
+export const Linkedin = styled(LinkedinSquare)`
+  width: 40px;
+  height: 40px;
+  margin-left: 5px;
+  color: white;
+  transition: color .2s linear;
+  
+  &:hover {
+    cursor: pointer;
+    color: #638DB3;
+  }
 `
 
+export const Medium = styled(MediumSquare)`
+  width: 40px;
+  height: 40px;
+  margin-left: 5px;
+  color: white;
+  transition: color .2s linear;
+  
+  &:hover {
+    cursor: pointer;
+    color: #638DB3;
+  }
+`
 
-export const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
+export const Insta = styled(InstagramAlt)`
+  width: 40px;
+  height: 40px;
+  margin-left: 5px;
+  color: white;
+  transition: color .2s linear;
+  
+  &:hover {
+    cursor: pointer;
+    color: #638DB3;
+  }
+`
+
+export const Face = styled(FacebookSquare)`
+  width: 40px;
+  height: 40px;
+  margin-left: 5px;
+  color: white;
+  transition: color .2s linear;
+  
+  &:hover {
+    cursor: pointer;
+    color: #638DB3;
+  }
+`
+
+export const Git = styled(Github)`
+  width: 35px;
+  height: 35px;
+  margin-left: 5px;
+  color: white;
+  transition: color .2s linear;
+  
+  &:hover {
+    cursor: pointer;
+    color: #638DB3;
+  }
+`
+
+export const Twit = styled(Twitter)`
+  width: 35px;
+  height: 35px;
+  margin-left: 5px;
+  color: white;
+  transition: color .2s linear;
+  
+  &:hover {
+    cursor: pointer;
+    color: #638DB3;
+  }
+`
