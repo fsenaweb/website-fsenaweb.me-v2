2f599b22cf1df9199c8ea6f463ea75214cd31d99
Matheus Ricelly <fsenaweb@gmail.com>
2020-06-24 15:52:06 -0300

    Conclusão dos ajustes de responsividade do componente de repositórios

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hability from "../components/Hability";
+import Repository from "../components/Repository";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hability/>
+    <Repository/>
   </Layout>
 )
 
