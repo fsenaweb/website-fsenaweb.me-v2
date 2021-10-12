f41963f426bd309e106d1c8c9233273120edb1c3
Matheus Ricelly <fsenaweb@gmail.com>
2021-10-12 17:31:44 -0300

    Melhorias nas informções do currículo

--- src/components/Hability/icon.js
+++ src/components/Hability/icon.js
@@ -1,78 +1,79 @@
-import iconPHP from '../../images/icon-PHP.png'
-import iconJS from '../../images/icon-JS.png'
-import iconVUE from '../../images/icon-VUE.png'
-import iconREACT from '../../images/icon-REACT.png'
-import iconLARAVEL from '../../images/icon-LARAVEL.png'
-import iconCI from '../../images/icon-CI.png'
-import iconDOCKER from '../../images/icon-DOCKER.png'
-import iconTS from '../../images/icon-typescript.png'
-import iconGIT from '../../images/icon-GIT.png'
-import iconNODE from '../../images/icon-NODE.png'
-import iconADONIS from '../../images/icon-ADONIS.png'
-import iconGASTBY from '../../images/icon-GASTBY.png'
-import iconGRQL from '../../images/icon-GRQL.png'
-import iconNUXT from '../../images/icon-NUXT.png'
-import iconQUASAR from '../../images/icon-QUASAR.png'
+import iconPHP from "../../images/icon-PHP.png"
+import iconJS from "../../images/icon-JS.png"
+import iconVUE from "../../images/icon-VUE.png"
+import iconREACT from "../../images/icon-REACT.png"
+import iconLARAVEL from "../../images/icon-LARAVEL.png"
+import iconCI from "../../images/icon-CI.png"
+import iconDOCKER from "../../images/icon-DOCKER.png"
+import iconTS from "../../images/icon-typescript.png"
+import iconGIT from "../../images/icon-GIT.png"
+import iconNODE from "../../images/icon-NODE.png"
+import iconADONIS from "../../images/icon-ADONIS.png"
+import iconGASTBY from "../../images/icon-GASTBY.png"
+import iconNEXT from "../../images/icon-NEXT.png"
+import iconGRQL from "../../images/icon-GRQL.png"
+import iconNUXT from "../../images/icon-NUXT.png"
+import iconQUASAR from "../../images/icon-QUASAR.png"
 
 export const icons = [
   {
-    title: 'PHP',
-    image: iconPHP,
-  },
-  {
-    title: 'Laravel',
-    image: iconLARAVEL,
-  },
-  {
-    title: 'Codeigniter',
-    image: iconCI,
-  },
-  {
-    title: 'Javascript',
+    title: "Javascript",
     image: iconJS,
   },
   {
-    title: 'TypeScript',
+    title: "TypeScript",
     image: iconTS,
   },
   {
-    title: 'React.js',
-    image: iconREACT,
-  },
-  {
-    title: 'Vue.js',
+    title: "Vue.js",
     image: iconVUE,
   },
   {
-    title: 'Node.js',
+    title: "React.js",
+    image: iconREACT,
+  },
+  {
+    title: "Node.js",
     image: iconNODE,
   },
   {
-    title: 'Adonis',
-    image: iconADONIS,
-  },
-  {
-    title: 'Gastby',
+    title: "Gastby",
     image: iconGASTBY,
   },
   {
-    title: 'Nuxt.js',
+    title: "Nuxt.js",
     image: iconNUXT,
   },
   {
-    title: 'GraphQL',
-    image: iconGRQL,
+    title: "Next.js",
+    image: iconNEXT,
   },
   {
-    title: 'Quasar',
+    title: "Adonis",
+    image: iconADONIS,
+  },
+  {
+    title: "Quasar",
     image: iconQUASAR,
   },
   {
-    title: 'Docker',
+    title: "PHP",
+    image: iconPHP,
+  },
+  {
+    title: "Laravel",
+    image: iconLARAVEL,
+  },
+  {
+    title: "Codeigniter",
+    image: iconCI,
+  },
+  {
+    title: "Docker",
     image: iconDOCKER,
   },
   {
-    title: 'Git',
+    title: "Git",
     image: iconGIT,
   },
 ]
